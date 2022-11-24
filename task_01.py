4ae04c13eacab3ac57e89d195a9a8dd2dd1637c8
grecha_grecha <ruspolak69@gmail.com>
2022-11-24 19:00:47 +0300

    some refactoring

--- task_01.py
+++ task_01.py
@@ -1,30 +1,25 @@
 from string import ascii_lowercase, digits
 
 
-class Solution:
-    def __init__(self):
-        self.s = None
-        self.rev_s = None
+def format_string(s) -> str:
+    """Возвращает строку без знаков препинания и пробелов"""
+    char_list = []
 
-    @staticmethod
-    def _format_string(string) -> str:
-        """Возвращает строку без знаков препинания и пробелов"""
-        char_list = []
+    for i in s:
+        if i in ascii_lowercase or i in digits:
+            char_list.append(i)
 
-        for i in string:
-            if i in ascii_lowercase or i in digits:
-                char_list.append(i)
+    return ''.join(char_list)
 
-        return ''.join(char_list)
 
-    def is_palindrome(self, string) -> bool:
-        self.s = str(string).lower()
+def is_palindrome(s) -> bool:
+    string = str(s).lower()
 
-        if not self.s.isalnum():
-            self.s = self._format_string(self.s)
+    if not string.isalnum():
+        string = format_string(string)
 
-        self.rev_s = self.s[::-1]
+    reversed_string = string[::-1]
 
-        if self.s == self.rev_s:
-            return True
-        return False
+    if string == reversed_string:
+        return True
+    return False
--- task_02.py
+++ task_02.py
@@ -1,16 +1,11 @@
-class Solution:
-    def __init__(self):
-        pass
+def coincidence(lst=None, rng=None) -> list:
+    if not lst or not rng:
+        return []
 
-    @staticmethod
-    def coincidence(lst=None, rng=None) -> list:
-        if not lst or not rng:
-            return []
+    in_range = []
+    for i in lst:
+        if isinstance(i, (int, float)):
+            if rng.start <= i < rng.stop:
+                in_range.append(i)
 
-        in_range = []
-        for i in lst:
-            if isinstance(i, (int, float)):
-                if rng.start <= i < rng.stop:
-                    in_range.append(i)
-
-        return in_range
+    return in_range
