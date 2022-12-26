6c978e5b84bb5d6393a6f5dd80ea2c10519f267b
grecha_grecha <ruspolak69@gmail.com>
2022-12-26 13:41:12 +0300

    completed task_08

---
+++ task_08.py
@@ -0,0 +1,22 @@
+from string import digits
+
+
+def multiply_numbers(*inputs):
+    if not inputs:
+        return None
+
+    ans = 1
+    digit_string = ''
+
+    for sym in str(inputs):
+        if sym in digits:
+            digit_string += sym
+
+    if not digit_string:
+        return None
+
+    for i in digit_string:
+        ans *= int(i)
+
+    print(ans)
+    return ans
