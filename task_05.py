424c4777c7e7c5d12d4ee86dcb955ef7598eb990
grecha_grecha <ruspolak69@gmail.com>
2022-12-30 08:59:41 +0300

    fixed task 05

--- task_05.py
+++ task_05.py
@@ -2,9 +2,10 @@ import datetime as dt
 
 
 def date_in_future(days: int) -> str:
+    date_format = '%d-%m-%Y %H:%M:%S'
     curr_time = dt.datetime.now()
 
     if not isinstance(days, int):
-        return curr_time.strftime('%Y-%m-%d %H:%M:%S')
+        return curr_time.strftime(date_format)
 
-    return (curr_time + dt.timedelta(days=days)).strftime('%Y-%m-%d %H:%M:%S')
+    return (curr_time + dt.timedelta(days=days)).strftime(date_format)
